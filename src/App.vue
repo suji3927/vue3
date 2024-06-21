bd0d854386d0663092df2d0c307d7aca3e300a36
suiji3927 <kim1989suji@gmail.com>
2024-06-21 14:43:59 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>영화 정보</h1>
+    <div>
+      <!-- :속성명="데이터"  -->
+      <h3 :style="textRed">{{ title }}</h3>
+      <p>개봉: {{ year }}</p>
+      <p>장르: {{ category }}</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() { // 상태변수
+      return {
+        title: "노량",
+        year: 2023,
+        category: "액션, 드라마",
+        textRed: "color: red",
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
