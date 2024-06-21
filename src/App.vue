1170a5b8c679c1f7c69290fe8025f9e92e96bf07
suiji3927 <kim1989suji@gmail.com>
2024-06-21 14:56:17 +0900

    v-for 반복문 문법

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
       <p>개봉: {{ year }}</p>
       <p>장르: {{ category }}</p>
     </div>
+<!-- 
+    <p>{{ foods[0] }}</p>
+    <p>{{ foods[1] }}</p>
+    <p>{{ foods[2] }}</p>
+     -->
+
+    <p v-for="(item, index) in foods" :key="index">{{ item }}</p>
 </template>
 
 <script>
@@ -13,6 +20,7 @@
     name: 'App',
     data() { // 상태변수
       return {
+        foods: ["김밥", "순대", "만두"],
         title: "노량",
         year: 2023,
         category: "액션, 드라마",
